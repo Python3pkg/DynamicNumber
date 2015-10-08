8f12eb25b9aec52adb93a10ff94901de0a98a485
Olivier Pieters <olivier.pieters@outlook.com>
2015-10-08 10:52:46 +0200

    Moved python-script and added PyPI distribution setup script.

---
+++ languages/python/setup.py
@@ -0,0 +1,76 @@
+"""A setuptools based setup module.
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path, remove
+from shutil import copyfile
+
+here = path.abspath(path.dirname(__file__))
+
+# Copy README from top dir
+copyfile('../../README.md','./README')
+
+# Get the long description from the README file
+with open(path.join(here, 'README'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='Dynamic Number',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version='0.1.1',
+
+    description='Dynamically export variables for reporting in LaTeX.',
+    long_description=long_description,
+
+    # The project's main homepage.
+    url='https://github.com/opieters/DynamicNumber#what-is-it',
+
+    # Author details
+    author='Olivier Pieters',
+    author_email='me@olivierpieters.be',
+
+    # Choose your license
+    license='MIT',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 5 - Production/Stable',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Science/Research',
+        'Topic :: Text Processing :: Markup :: LaTeX',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+
+    # What does your project relate to?
+    keywords='dynamic number latex typesetting reporting',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+)
+
+remove('./README')
