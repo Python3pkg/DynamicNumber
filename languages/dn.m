c7e08c8998a16e764f80f9cbfe44e564021f2227
Olivier Pieters <olivier.pieters@outlook.com>
2015-10-27 11:51:41 +0100

    Bug fixes.

--- languages/dn.m
+++ languages/dn.m
@@ -1,11 +1,20 @@
 classdef dn
+    % MATLAB scripting side to Dynamic Number (dn) package
+    % Defines a MATLAB class that defines IO.
+
     properties
-        name
-        filename
-        directory
+        name       % dn list name
+        filename   % filename (with extension, but without directory)
+        directory  % file directory with filename
     end
     methods
         function obj = dn(list_name, varargin)
+            % Create a dn object
+            %
+            %  list = dn('name') creates the list name in the current directory.
+            %  list = dn('name','..') creates the list name one directory above
+            %    this directory.
+
             if (nargin<1) || (nargin>2)
                 error('Incorrect number of input arguments.')
             end
@@ -29,14 +38,20 @@ classdef dn
             else
                 error('Provide a MATLAB string as list directory')
             end
-            
+
             f = fopen(obj.directory,'w');
             fprintf(f,'\\dndeclare{%s}\n',obj.name);
             fprintf(f,'\\dnsetcurrent{%s}\n',obj.name);
             fclose(f);
-            
+
         end
         function[] = add(obj, name, value, varargin)
+            % Add a value to the liist
+            %
+            %  list.add('A',N) adds the value of N to key A.
+            %  list.add('B',N,'\radians') adds the command \unit{N}{\radians} to
+            %    the key B.
+            
             if isnumeric(value)
                 value = num2str(value);
             end
@@ -50,7 +65,7 @@ classdef dn
                     fclose(f);
                 case 4
                     f = fopen(obj.directory,'a');
-                    fprintf(f,'\\pgfkeys{/dynamicnumber/%s/%s = \\unit{%s}{%s}}\n',obj.name,name,value,unit);
+                    fprintf(f,'\\pgfkeys{/dynamicnumber/%s/%s = \\unit{%s}{%s}}\n',obj.name,name,value,varargin{1});
                     fclose(f);
                 otherwise
                     error('Incorrect number of arguments.')
